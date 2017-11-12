360ff7854e2689cd74d902be58fcce6c47781d1d
malloryb <mallorylbarnes@gmail.com>
2017-11-12 15:51:11 -0700

    Adding in info for table 1

--- 10_Figure_4_Table_S1.R
+++ 10_Figure_4_Table_S1.R
@@ -70,3 +70,47 @@ c2 <- ggplot(Corrs, aes(y=Jmax_R_squared, x=reorder(Index, order)))+
 tiff(filename = "Figure_4_done.tiff", width=13, height=11, unit="cm", res = 300)
 multiplot(c1,c2)
 dev.off()
+
+#Information for Table 1 
+#hyperspectral data - create "unique id" (same as in 'merged')and then average by unique ID
+#Then will be able to merge with A/Ci and Climate files
+#'uniqueID' takes form: a18-2016-06-08 
+merged <- read.csv("C:/Users/rsstudent/Dropbox/Drought_Expt_2016/Merged_data_to_analyze_3_6_2017.csv")
+hyperspec <- read.csv("C:/Users/rsstudent/Dropbox/Drought_Expt_2016/Processed_Hyperspec_Files_4_25_2017.csv")
+hyperspec$uniqueID <- paste(tolower(hyperspec$ID), hyperspec$date, sep='-')
+str(hyperspec)
+#want means of VIs by uniqueID - use ddply
+hyperspec_avg <- ddply(hyperspec,~uniqueID,summarise, SRcarter=mean(SRcarter), NDVI=mean(NDVI), NDVI_mod=mean(NDVI_mod, na.rm=TRUE), PRI=mean(PRI), NDWI=mean(NDWI), Datt4=mean(Datt4), Vogelmann2=mean(Vogelmann2), Maccioni=mean(Maccioni), Double_Difference=mean(Double_Difference), Vogelmann1=mean(Vogelmann1), mSR705=mean(mSR705), SR3=mean(SR3), SR4=mean(SR4), SR1=mean(SR1), Gitelson=mean(Gitelson), SR2=mean(SR2), SIPI=mean(SIPI), mNDVI=mean(mNDVI), mSRCHL=mean(mSRCHL))
+#merge hyperspec and a/ci data
+anti_join(merged, hyperspec_avg, by="uniqueID")
+hyperspec_avg$uniqueID
+all_data <- merge(hyperspec_avg, merged, by="uniqueID")
+str(all_data)
+#clean up "all_data"
+#delete X.2, X.1, and X columns (what even are these?)
+all_data <- subset(all_data, select=-c(X,X.1))
+#giant correlation matrix to clipboard for quick glance
+#Saved as 'corr matrix' in the Paper_2 folder in dropbox
+
+check_corrs <- function(xx, ii)
+{
+  CORV=cor(xx$Vcmax, xx[[ii]], use="pairwise.complete.obs")
+  SIGV=cor.test(xx$Vcmax,xx[[ii]], use="pairwise.complete.obs")$p.value
+  CORJ=cor(xx$Jmax, xx[[ii]], use="pairwise.complete.obs")
+  SIGJ=cor.test(xx$Jmax,xx[[ii]], use="pairwise.complete.obs")$p.value
+  RSQV=(CORV*CORV)
+  RSQJ=(CORJ*CORJ)
+  return(data.frame(RSQV, SIGV, RSQJ, SIGJ))
+  
+}
+str(all_data)
+
+check_corrs(all_data, "SR1")
+check_corrs(all_data, "NDWI")
+check_corrs(all_data, "SIPI")
+
+check_corrs(all_data, "PRI")
+check_corrs(all_data, "Gitelson")
+check_corrs(all_data, "mNDVI")
+
+#etc...
