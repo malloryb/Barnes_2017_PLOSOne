38a446f57a86d4bd5349436d67e79b093ecbce0d
malloryb <mallorylbarnes@gmail.com>
2017-11-12 14:27:14 -0700

    Cleaned up -> may need to revert to previous version

--- 06_Figure_1.R
+++ 06_Figure_1.R
@@ -22,7 +22,7 @@ library(gridBase)
 #Desired appearance
 grid.newpage()
 #Figure 1------------------------------------------------------------
-Plot_data <- read.csv("C:/Users/Mallory/Dropbox/Drought_Expt_2016/all_data_3_6_2017.csv")
+Plot_data <- read.csv("C:/Users/rsstudent/Dropbox (Personal)/Drought_Expt_2016/all_data_3_6_2017.csv")
 str(Plot_data)
 #Cleanup data file:
 #Format Dates properly: 
@@ -37,41 +37,11 @@ Plot_data <- Plot_data[-c(38),]
 
 #Figure 1: Multipanel stacked figure; climate/physiology
 #Now for the climate data: Want a free axis graph with precip and temp and then another with VPD (bars)
-plot_climate <- read.csv("C:/Users/Mallory/Dropbox (Dissertation Dropbox)/Barnes_PLOSOne_Poplar/plot_climate.csv")
-str(plot_climate)
+plot_climate <- read.csv("C:/Users/rsstudent/Dropbox (Dissertation Dropbox)/Barnes_PLOSOne_Poplar/plot_climate.csv")
 plot_climate$Date <- as.Date(plot_climate$Date)
 plot_climate <- subset(plot_climate, Date > "2016-05-23")
-dev.off()
-plot.new()
-par(mar = c(5,8,2,5))
-par(new=T)
-rect(as.Date("2016-06-02", "%Y-%m-%d"), 0, as.Date("2016-06-07", "%Y-%m-%d"), 50, col = "lightblue")
-rect(as.Date("2016-06-19", "%Y-%m-%d"), 0, as.Date("2016-06-20", "%Y-%m-%d"), 50, col = "lightblue")
-
-par(new=T)
-with(plot_climate, plot(Date, Temp, type="l", col="tomato", 
-             ylab="Temp (C)",
-             ylim=c(20,40)))
-
-par(new = T)
-with(plot_climate, plot(Date, VPD, type="l", col="mediumvioletred", axes=F, xlab=NA, ylab=NA, cex=1)) 
-axis(side = 4)
-mtext(side = 4, line = 1.5, "VPD (KPa)")
-
-par(new=T)
-with(plot_climate, barplot(Precip, col="blue", axes=F, xlab=NA, ylab=NA, ylim=c(0,40)))
-axis(side=2, ylim=c(0, 40), lwd=1, line=3)
-mtext(side=2, line=4, "Precip (mm)")
 
 
-plot(w)
-
-#For the boxplots: 
-#Start here?!----------------------------------
-#still can't save it properly
-#Physiological data
-#Water Potential Data 
-str(Plot_data)
 dev.off()
 
 
